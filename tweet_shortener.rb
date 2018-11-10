b65b329d7b0fee22fce0f4fb0874c0d10a83d17d
Roman Pirag <mr.anadale@yahoo.com>
2018-11-10 18:16:37 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -35,3 +35,13 @@ def selective_tweet_shortener(tweet)
     tweet 
   end
 end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).length > 140
+     puts word_substituter(tweet)[0..136] + "..."
+  else 
+     puts tweet 
+  end
+end
+
+
