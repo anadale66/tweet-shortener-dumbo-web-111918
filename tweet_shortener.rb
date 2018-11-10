bede1874a80a9ac19e660a1702bdb864c9d755b5
Roman Pirag <mr.anadale@yahoo.com>
2018-11-10 18:00:17 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,6 +16,8 @@ def word_substituter(tweet)
   tweet.split.map do |word| 
     if dictionary.keys.include?(word.downcase)
        word = dictionary[word.downcase]
+    else 
+       word 
     end
   end.join(" ")
 end
