6b13c688074d47ca29bf55bea19d39297522f977
Roman Pirag <mr.anadale@yahoo.com>
2018-11-10 17:41:11 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,14 +13,15 @@ dictionary = {
 end
 
 def word_substituter(tweet)
-  tweet.split.collect do |word|
+  foo = tweet.split.each do |word|
     if dictionary.keys.include?(word.downcase)
       word = dictionary[word.downcase]
     else
       word
     end
     
-  end.join(" ")
+  end
+  foo.join(" ")
 end
 
 def bulk_tweet_shortener(tweets)
